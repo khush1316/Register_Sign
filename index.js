f254a7405cc23042d076728ab1b840a245707ef9
KHUSHAL <118012624+khush1316@users.noreply.github.com>
2024-03-21 00:29:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const db = require('./routes/db-config');
+const cookie = require('cookie-parser');
+const app = express();
+const PORT = process.env.PORT || 8080
+
+app.use("/js", express.static(__dirname + "/public/js"))
+app.use("/css", express.static(__dirname + "/public/css"))
+app.set("view engine", "ejs");
+app.set("views","./views");
+app.use(cookie()); 
+
+app.use(express.json());
+
+db.connect((err) => {
+    if(err) throw err;
+    console.log("Database Connected...");
+})
+
+
+app.use("/",require("./routes/pages"));
+app.use('/api', require('./controllers/auth'));
+app.listen(PORT);
